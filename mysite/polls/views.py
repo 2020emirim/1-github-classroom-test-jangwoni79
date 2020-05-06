c64bba8ee12407ccdbc408a1eaec5bfa2042ac6b
jangwoni79 <s2018w17@e-mirim.hs.kr>
2020-05-07 08:59:26 +0900

    polls/views, tmplates detail, result, vote

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -15,9 +15,7 @@ def index(request):
 
 
 def detail(request, question_id):
-
     question = get_object_or_404(Question, pk=question_id)
-
     return render(request, 'polls/detail.html', {'question': question})
 
 
@@ -31,7 +29,6 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
